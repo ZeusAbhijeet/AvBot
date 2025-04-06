3f171ced97ce5ff85bdcf0db1571a6e5419a78ae
Abhijeet Singh <62468954+ZeusAbhijeet@users.noreply.github.com>
2025-04-06 23:43:59 +0530

    Initial Commit

---
+++ __main__.py
@@ -0,0 +1,10 @@
+import os
+from bot import bot
+
+if __name__ == "__main__":
+    if os.name != "nt":
+        import uvloop
+
+        uvloop.install()
+
+    bot.run()
---
+++ bot.py
@@ -0,0 +1,84 @@
+import os
+
+import hikari
+import arc
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Setup the bot
+# I am sleepy, so I am enabling all intents. DON'T DO THIS.
+# This bot doesn't need all intents. Only requires Server Members. I am just too tired to look up which ones exactly :moai:
+bot = hikari.GatewayBot(
+    token=TOKEN,
+    intents=hikari.Intents.ALL,
+    allow_color=True
+)
+
+# Setup the arc client object
+client: arc.GatewayClient = arc.GatewayClient(bot)
+
+# client.load_extensions_from("exts")
+
+@client.add_startup_hook
+async def startup_hook(client: arc.GatewayClient) -> None:
+    logging.info("Bot is starting up")
+    await client.app.update_presence(
+        status=hikari.Status.ONLINE,
+        activity=hikari.Activity(
+            name=f"your reactions",
+            type=hikari.ActivityType.WATCHING
+        )
+    )
+
+@client.include
+@arc.with_hook(arc.has_permissions(hikari.Permissions.ADMINISTRATOR))
+@arc.slash_command(
+    name="load_extension",
+    description="Load extensions to the bot. Admin Command.",
+    default_permissions=hikari.Permissions.ADMINISTRATOR
+)
+async def load_ext_slash(
+    ctx: arc.GatewayContext,
+    extension: arc.Option[str,
+        arc.StrParams(
+            "Extension to load",
+            choices={"Utils": "exts.utils", "Verification": "exts.verification", "Auto Announcement": "exts.auto_announcement"}
+        )
+    ]
+) -> None:
+    await ctx.respond(f"Selection: {extension}")
+
+
+@client.include
+@arc.with_hook(arc.has_permissions(hikari.Permissions.ADMINISTRATOR))
+@arc.slash_command(
+    name="unload_extension",
+    description="Unload extensions to the bot. Admin Command.",
+    default_permissions=hikari.Permissions.ADMINISTRATOR
+)
+async def unload_ext_slash(
+    ctx: arc.GatewayContext,
+    extension: arc.Option[str,
+    arc.StrParams(
+        "Extension to unload",
+        choices={"Utils": "exts.utils", "Verification": "exts.verification", "Auto Announcement": "exts.auto_announcement"}
+    )]
+) -> None:
+    await ctx.respond(f"Selection: {extension}")
+
+
+@client.include
+@arc.with_hook(arc.has_permissions(hikari.Permissions.ADMINISTRATOR))
+@arc.slash_command(
+    "shutdown",
+    "Turn the bot off. Admin command. If you can see this and are not an admin, ZeusAbhijeet screwed up",
+    default_permissions=hikari.Permissions.ADMINISTRATOR
+)
+async def shutdown_slash(
+    ctx: arc.GatewayContext
+) -> None:
+    await ctx.respond("Bot is shutting down")
+    await bot.close()
